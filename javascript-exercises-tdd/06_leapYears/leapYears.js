86b3574aa104230e9da2ed13fbe8cb18ebee4f07
David Centurion <dencen9@gmail.com>
2022-02-02 04:57:02 -0300

    finished 07exerciseTDD

--- javascript-exercises-tdd/06_leapYears/leapYears.js
+++ javascript-exercises-tdd/06_leapYears/leapYears.js
@@ -1,7 +1,9 @@
 // Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
 
-// check if year is divisible by 4, yes if leap 
 // if year if divisible for 100 and 400, yes is leap
+// if year is only divisible by 100, false, is not leap
+// check if year is divisible by 4, yes if leap 
+// else return false
 
 
 const leapYears = function(year) {
