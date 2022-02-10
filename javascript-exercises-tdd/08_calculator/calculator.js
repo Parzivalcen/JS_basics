e4912117c04ecd74088c2f14b7c768f47fc08951
David Centurion <dencen9@gmail.com>
2022-02-10 04:30:09 -0300

    finished exercise 08

--- javascript-exercises-tdd/08_calculator/calculator.js
+++ javascript-exercises-tdd/08_calculator/calculator.js
@@ -1,26 +1,61 @@
-const add = function() {
-	
+// add argument
+// gonna have to use ...args probably
+// check if the array is empty
+const add = function (...args) {
+  let sum = 0;
+  for (let i of args) {
+    sum += i;
+  }
+
+  return sum;
 };
 
-const subtract = function() {
-	
+console.log(add(2, 3));
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (...args) {
+  let sum = args[0];
+  if (sum.length === 0) {
+    return 0;
+  }
+  sum = sum.reduce((n, a) => {
+    return n + a;
+  });
+
+  return sum;
+};
+console.log(sum([1, 3]));
+
+const multiply = function (...args) {
+  let mult = args[0];
+  if (mult.length === 0) {
+    return 0;
+  }
+  mult = mult.reduce((n, a) => {
+    return n * a;
+  });
+
+  return mult;
 };
 
-const multiply = function() {
-
+const power = function (a, b) {
+  return a ** b;
 };
 
-const power = function() {
-	
+//
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+  for (let i = n - 1; i > 0; i--) {
+    n *= i;
+    console.log(i);
+  }
+  return n;
 };
-
-const factorial = function() {
-	
-};
+console.log(factorial(1));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +64,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
