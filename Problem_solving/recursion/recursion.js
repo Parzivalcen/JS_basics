ba04685c467acc93379eeeb5a9dc30992529af8f
David Centurion <dencen9@gmail.com>
2022-08-25 18:32:26 -0400

    parse multidimensional array

--- Problem_solving/recursion/recursion.js
+++ Problem_solving/recursion/recursion.js
@@ -1,16 +1,45 @@
-let six = productOfArray([1,2,3]) // 6
-let sixty = productOfArray([1,2,3,10]) // 60
+let nestedObject = {
+  data: {
+      info: {
+          stuff: {
+              thing: {
+                  moreStuff: {
+                      magicNumber: 44,
+                      something: 'foo2'
+                  }
+              }
+          }
+      }
+  }
+}
 // non recursive factorial
 // sumRange(3);
-console.log(six, sixty);
-function productOfArray(arr){
-  let copy = arr.slice() || copy;
+let seven = totalInts([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
 
-  if(copy.length === 0) return 1;
-  return copy.shift() * productOfArray(copy);
+console.log(seven);
+
+function totalInts(arr){
+  if(arr.length===0) return 0;
+  let count = 0;
+  let first = arr.shift();
+
+  if(Array.isArray(first)){
+    console.log('arr', first);
+    count += totalInts(first);
+  }
+  if(Number.isInteger(first)){
+    console.log('n', first);
+    count ++;
+  }
+
+  return count + totalInts(arr);
 }
 
-
-// if arr.length == 0, return
-// return arr.shift * productOfArray(arr);
-
+// base case; if arr.length == 0, return 0;
+// count = 0;
+// get first elements of arr every call, first = arr.shift();
+// if first is array
+//    count += totalints(first);
+// if first is num
+//    count += 1
+// return count += totalInts(arr);
