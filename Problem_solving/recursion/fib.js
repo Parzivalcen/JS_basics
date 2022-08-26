7a11ae7f3c78630c6407e598d191689e03ef305f
David Centurion <dencen9@gmail.com>
2022-08-26 16:06:52 -0400

    ffib recursion

--- Problem_solving/recursion/fib.js
+++ Problem_solving/recursion/fib.js
@@ -1,4 +1,37 @@
 console.log(fibsRec(8));
+console.log(fibs(8));
+
+
+
+
+function fibs(n){
+  let arr = []
+  let fib0 = 0;
+  let fib1 = 1;
+  let fib = 0;
+
+  while(arr.length<n){
+    arr.push(fib)
+    fib0 = fib1;
+    fib1 = fib;
+    fib=  fib0 + fib1;
+
+  }
+  return arr;
+}
+// arr = []
+// fib0 = 0
+// fib1 = 1
+// fib = 0
+
+// while(arr length < n)
+//    arr.push(fib)
+//    fib0 = fib1
+//    fib1 = fib0
+//    fib = fib0 + fib1
+// return fib
+
+
 
 function fibsRec (n, arr = [0, 1]){
   if(arr.length === n) return arr;
